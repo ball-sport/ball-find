63a6ce505efefc68df20b1d9f128e1351775f746
ball-sport <sophie.bgh1987@gmail.com>
2025-04-30 00:31:50 +0100

    Update streamlit_app.py
    add file uploader

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,4 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
-
-    # در اینجا می‌تونی پردازش مدل رو اضافه کنی
-    st.success("تصویر با موفقیت آپلود شد. مدل به‌زودی پردازش را شروع می‌کند.")
+    st.success("تصویر با موفقیت آپلود شد.")
